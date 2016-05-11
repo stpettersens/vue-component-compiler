8bd9a57ceef6fdcb261513d7d879d593b288d7ce
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-05-11 15:35:07 +0100

    Update cli.ts

--- cli.ts
+++ cli.ts
@@ -1,2 +1,9 @@
+/*
+	Vue component compiler.
+	Unoffical "compiler" for Vue.js components written in a class-based style.
+	Copyright 2015-2016 Sam Saint-Pettersen.
+	Released under the MIT License.
+*/
+
 import VueComponentCompiler = require('./vuecc');
-new VueComponentCompiler(process.argv[2], process.argv[3], process.argv);
+new VueComponentCompiler(process.argv[1], process.argv[2], process.argv[3], process.argv);
